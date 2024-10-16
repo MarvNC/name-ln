082c7dad05b8a0c0fd0ffce724b7396c561f4c98
MarvNC <rotrobmin@gmail.com>
2024-10-16 14:34:30 -0700

    Add tests

--- src/name.ts
+++ src/name.ts
@@ -1,4 +1,4 @@
-function generateName(bookData: {
+export type BookData = {
   author: string;
   date: Date | string;
   title: string;
@@ -6,13 +6,19 @@ function generateName(bookData: {
   label?: string;
   retailer?: string;
   extra?: string;
-}): string {
+};
+
+export function generateName(bookData: BookData): string {
   // [Author] YYYYMMDD - v01 Title (Publisher - Label) (Retailer) [Extra Information] {Revision}
   const dateString =
     bookData.date instanceof Date
       ? bookData.date.toISOString().slice(0, 10).replace(/-/g, '')
       : bookData.date;
-  return `[${bookData.author}] ${dateString} - v01 ${bookData.title} (${
+
+  // Remove spaces and full-width spaces from author name
+  const cleanAuthor = bookData.author.replace(/[ 　]/g, '');
+
+  return `[${cleanAuthor}] ${dateString} - v01 ${bookData.title} (${
     bookData.publisher
   }${bookData.label ? ` - ${bookData.label}` : ''})${
     bookData.retailer ? ` (${bookData.retailer})` : ''
--- src/name_test.ts
+++ src/name_test.ts
@@ -1,1 +1,48 @@
-// [白石定規] 20241013 - 魔女の旅々２３ (SBクリエイティブ - ＧＡノベル) (Bookwalker)
+import { assertEquals } from 'https://deno.land/std@0.106.0/testing/asserts.ts';
+import { generateName, type BookData } from './name.ts';
+
+Deno.test('generateName test with string date', () => {
+  const bookData: BookData = {
+    author: '白石定規',
+    date: '20241013',
+    title: '魔女の旅々２３',
+    publisher: 'SBクリエイティブ',
+    label: 'ＧＡノベル',
+    retailer: 'Bookwalker',
+  };
+  const expected =
+    '[白石定規] 20241013 - v01 魔女の旅々２３ (SBクリエイティブ - ＧＡノベル) (Bookwalker)';
+  const result = generateName(bookData);
+  assertEquals(result, expected);
+});
+
+Deno.test('generateName test with Date object', () => {
+  const bookData: BookData = {
+    author: '白石定規',
+    date: new Date('2024-10-13'),
+    title: '魔女の旅々２３',
+    publisher: 'SBクリエイティブ',
+    label: 'ＧＡノベル',
+    retailer: 'Bookwalker',
+  };
+  const expected =
+    '[白石定規] 20241013 - v01 魔女の旅々２３ (SBクリエイティブ - ＧＡノベル) (Bookwalker)';
+  const result = generateName(bookData);
+  assertEquals(result, expected);
+});
+
+Deno.test('generateName test with cleaned author string', () => {
+  const bookData: BookData = {
+    author: '大木戸　いずみ',
+    date: '20241015',
+    title:
+      '歴史に残る悪女になるぞ ７　悪役令嬢になるほど王子の溺愛は加速するようです！【電子特典付き】',
+    publisher: 'KADOKAWA',
+    label: 'ビーズログ文庫',
+    retailer: 'Bookwalker',
+  };
+  const expected =
+    '[大木戸いずみ] 20241015 - v01 歴史に残る悪女になるぞ ７　悪役令嬢になるほど王子の溺愛は加速するようです！【電子特典付き】 (KADOKAWA - ビーズログ文庫) (Bookwalker)';
+  const result = generateName(bookData);
+  assertEquals(result, expected);
+});
