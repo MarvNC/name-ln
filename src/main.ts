e9e5a5cee4422e4720cb638788eb3635c01791a7
MarvNC <rotrobmin@gmail.com>
2024-10-16 21:14:14 -0700

    Add build script, help message

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,48 @@ import type { EpubFileEntry } from './types.ts';
 import { isEpub } from './util/isEpub.ts';
 import { handleFile } from './util/handleFile.ts';
 
+function printHelp() {
+  console.log(`
+Usage: deno run --allow-read --allow-write main.ts [options] <files or directories>
+
+Options:
+  --retailer, -r  Specify the retailer name
+  --extra, -e     Specify extra information
+
+Examples:
+  deno run --allow-read --allow-write main.ts --retailer Bookwalker --extra Promo file1.epub file2.epub
+  deno run --allow-read --allow-write main.ts -r Bookwalker -e Promo dir1 dir2
+  `);
+}
+
 if (import.meta.main) {
+  main();
+}
+
+async function main() {
   // read args
   const args = Deno.args;
   if (args.length === 0) {
-    console.log('Please provide a file to rename');
+    printHelp();
     Deno.exit(1);
   }
   // Args: retailer, extra, also shortened versions
   const parsedArgs = parseArgs(Deno.args, {
-    string: ['retailer', 'extra', 'r', 'e'],
+    string: ['retailer', 'extra'],
+    alias: {
+      retailer: 'r',
+      extra: 'e',
+      help: 'h',
+    },
   });
 
-  const retailer = parsedArgs.retailer || parsedArgs.r;
-  const extra = parsedArgs.extra || parsedArgs.e;
+  if (parsedArgs.help) {
+    printHelp();
+    Deno.exit(0);
+  }
+
+  const retailer = parsedArgs.retailer;
+  const extra = parsedArgs.extra;
   const filesOrDirs = parsedArgs._;
 
   const epubFilesToHandle: EpubFileEntry[] = [];
